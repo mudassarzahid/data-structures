833f6720b6b41d4e3e597df18a646df55a1c9eb4
Mudassar Zahid <mudassar.ahmad.zahid@gmail.com>
2021-04-05 16:57:26 +0200

    Add LinkedList

---
+++ LinkedList/LinkedList.py
@@ -0,0 +1,88 @@
+class Node():
+  def __init__(self, data):
+    self.data = data
+    self.next_node = None
+
+
+class LinkedList():
+  def __init__(self, head=None):
+    self.head = head
+
+  def length(self):
+    count = 0
+    runner = self.head
+    while runner:
+      runner = runner.next_node
+      count += 1
+    return count
+
+  def view(self):
+    string = ''
+    runner = self.head
+    while runner:
+      string += "({})".format(runner.data)
+      runner = runner.next_node
+      if runner:
+        string += ' --> '
+    return string
+
+  def tail(self):
+    runner = self.head
+    while runner.next_node:
+      runner = runner.next_node
+    return runner
+
+  def insert(self, data, position=0):
+    # edge case: negative position
+    if position < 0:
+      return 'Invalid position!'
+
+    new_node = Node(data)
+
+    # insert at head, default
+    if position == 0:
+      old_head = self.head
+      self.head = new_node
+      new_node.next_node = old_head
+
+    # insert elsewhere
+    else:
+      following_node = self.head
+      for i in range(position):
+        previous_node = following_node
+        if previous_node == None:
+          return 'Invalid position!'
+        following_node = following_node.next_node
+
+      previous_node.next_node = new_node
+      new_node.next_node = following_node
+
+    return "Inserted node with value = {} at position = {}.".format(data, position)
+
+  def append(self, data):
+    new_node = Node(data)
+    self.tail().next_node = new_node
+
+    return "Appended node with value = {} to LinkedList.".format(data)
+
+  def remove(self, position=0):
+     # edge case: negative position
+    if position < 0:
+      return 'Invalid position!'
+
+    # remove head, default
+    if position == 0:
+      self.head = self.head.next_node
+
+    # remove elsewhere
+    else:
+      following_node = self.head
+      for i in range(position):
+        previous_node = following_node
+        if following_node.next_node == None:
+          return 'Invalid position!'
+        following_node = following_node.next_node
+
+      previous_node.next_node = following_node.next_node
+
+    return "Removed node at position {} (value = {}).".format(position, previous_node.data)
---
+++ LinkedList/LinkedList_example.py
@@ -0,0 +1,23 @@
+from LinkedList import Node, LinkedList
+
+a = Node({'a': 'b'})
+b = Node('c')
+c = Node(4)
+d = Node(3)
+e = Node(['d', 'e'])
+
+a.next_node = b
+b.next_node = c
+c.next_node = d
+d.next_node = e
+
+ll = LinkedList(a)
+
+print('LinkedList: {}.'.format(ll.view()))
+print('LinkedList length: {}.'.format(ll.length()))
+print('Tail value: {}.'.format(ll.tail().data))
+print(ll.insert(['z', 'a'], 3))
+print(ll.append(10))
+print('LinkedList: {}.'.format(ll.view()))
+print(ll.remove(4))
+print('LinkedList: {}.'.format(ll.view()))
